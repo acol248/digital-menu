94048f35420ad4501c1a7d0cdfdc5efab9abad19
acol248 <acol248@gmail.com>
2024-04-13 08:17:36 +0100

    🚧 Example for MenuSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // components
 import ThemeWrapper from "./components/ThemeWrapper";
+import MenuSection from "./components/MenuSection";
 
 // styles
 import useClassList, { mapClassesCurried } from "@blocdigital/useclasslist";
@@ -8,13 +9,24 @@ import "./interface/variables.css";
 
 const mc = mapClassesCurried(maps, true) as (c: string) => string;
 
+const exampleData = [
+  {
+    name: "Bolognese",
+    description:
+      "A sweet and tomatoey bolognese containing beef and pork mince, courgette, peppers, red onions and assorted seasoning.",
+    price: 20,
+  },
+];
+
 function App() {
   const classList = useClassList({ defaultClass: "app", maps, string: true }) as string;
 
   return (
     <ThemeWrapper value={"light"}>
       <div className={classList}>
-        <h1 className={mc('app__name')}>NAME</h1>
+        <h1 className={mc("app__name")}>NAME</h1>
+
+        <MenuSection title="Example" items={exampleData} />
       </div>
     </ThemeWrapper>
   );
